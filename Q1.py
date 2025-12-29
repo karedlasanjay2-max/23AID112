cb628954463e6ed14e6e01925c2dfb8f07ddd224
karedlasanjay2-max <karedlasanjay2@gmail.com>
2025-12-29 23:45:10 +0530

    Q1.py

---
+++ Q1.py
@@ -0,0 +1,9 @@
+age = 18
+height = 5.8
+name = "Sidhu"
+are_you_student = True
+
+print("Name       :", name)
+print("Age        :", age)
+print("Height     :", height)
+print("Student    :", are_you_student)
